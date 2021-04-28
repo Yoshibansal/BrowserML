3de595d4a4038281ba2442b87644b9ff70a6f501
Yoshi Bansal <56353404+Yoshibansal@users.noreply.github.com>
2021-04-28 14:50:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+//since training model takes varient time we define a async funtion
+async function doTraining(model){
+    const history =
+          await model.fit(xs, ys,
+                { epochs: 500,
+                  callbacks:{
+                      onEpochEnd: async(epoch, logs) =>{
+                          console.log("Epoch:"
+                                      + epoch
+                                      + " Loss:"
+                                      + logs.loss);
+
+                      }
+                  }
+                });
+}
+
+
+
+//Disabled button until model is trained successfully
+var button1 = document.getElementById("btn");
+button1.disabled = true;
+
+
+
+
+////////////////////////////// MODEL //////////////////////////////
+
+
+//model architecture
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+
+
+//compile
+model.compile({loss:'meanSquaredError',
+               optimizer:'sgd'});
+
+model.summary();
+
+
+
+//defining data
+const xs = tf.tensor2d([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], [6, 1]);
+const ys = tf.tensor2d([-3.0, -1.0, 2.0, 3.0, 5.0, 7.0], [6, 1]);
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+
+//training and enabling the button after it
+doTraining(model).then(() => {
+    alert("Training done!!!");
+    button1.disabled = false;
+});;
+
+
+
+
+//click the button on a press of enter in input field
+document.getElementById("testInput")
+    .addEventListener("keyup", function(e) {
+        if (e.keyCode === 13) {
+            document.getElementById("btn").click();
+        }
+    });
+
+
+
+    
+//predict function
+function predict(){
+    var input_val = parseInt(document.getElementById("testInput").value);
+    document.getElementById("result").innerHTML = model.predict(tf.tensor2d([input_val], [1,1]));
+}
